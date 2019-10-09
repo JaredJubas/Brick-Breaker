c1d5d2c45f16279515f53756c465421342fe546d
jjmalz <50532220+jjmalz@users.noreply.github.com>
2019-10-09 17:51:38 -0400

    Improved documentation and moved functions

--- script.js
+++ script.js
@@ -1,33 +1,126 @@
+// The canvas
 var canvas = document.getElementById("myCanvas");
+
+// The context
 var ctx = canvas.getContext("2d");
+
+// Text variables
 var startText = document.getElementById("start");
 var loseText = document.getElementById("lose");
 var winText = document.getElementById("win");
+
+// Platform variables
 var platformX
 var platformY
+var pWidth = 70;
+
+// Variables for if the paddle should be moving
+var left = 0;
+var right = 0;
+
+// Ball variables
 var x;
 var y;
 var radius;
+
+// How the ball moves in x and y direction
 var dx;
 var dy;
+
+// Brick variables
 var bricks = [];
 var bWidth = 80;
 var bHeight = 20;
-var pWidth = 70;
-var interval;
+
+// If the game has started yet
 var started = 0;
-var left = 0;
-var right = 0;
+
+// Interval to keep the screen constantly updating
+var interval;
+
+// Key listeners
 window.addEventListener("keydown", keyActionDown);
 window.addEventListener("keyup", keyActionUp);
 
+// Setup the game
+setGame()
+
+function setGame() {
+    // Setup the game 
+
+    x = canvas.width / 2;
+    y = canvas.height - 200;
+    radius = 10;
+    dx = 0;
+    dy = 4;
+    platformX = canvas.width / 2 - 30;
+    platformY = canvas.height - 18;
+    setBricks()
+    interval = setInterval(draw, 10);
+}
+
 function setBricks() {
+    // Initialize the bricks variabe
+
     for (let i = 0; i < 20; i++) {
+        // format of bricks is: x position, y position, and 1 if it has not been hit and 0 if it has
         bricks[i] = [0, 0, 1];
     }
 }
 
+function draw() {
+    // Draw everything on the screen
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawRectangles();
+    drawPlatform();
+    detectCollision();
+
+    if (started) {
+        if (y + dy + radius == platformY) {
+            if (platformX - 2 * radius < x + dx && x + dx < platformX + pWidth + radius && y + dy + radius == platformY) {
+                dy = -dy;
+                if (dx == 0) {
+                    dx = 2;
+                }
+            }
+        }
+        else {
+            if (x + dx > canvas.width - radius || x + dx < radius) {
+                dx = -dx;
+            }
+            if (y + dy < radius) {
+                dy = -dy;
+            }
+        }
+        if (y >= canvas.height + radius) {
+            announceWin(0)
+        }
+        x += dx;
+        y += dy;
+    }
+    if (left) {
+        platformX = Math.max(platformX - 6, 0);
+    }
+    else if (right) {
+        platformX = Math.min(platformX + 6, canvas.width - pWidth);
+    }
+}
+
+function drawBall() {
+    // Draw the ball
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2*Math.PI);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function drawRectangles() {
+    // Draw the bricks
+
     for (let i = 0; i < bricks.length; i++) {
         if (bricks[i][2] == 1) {
             let brickX = 0;
@@ -66,6 +159,8 @@ function drawRectangles() {
 }
 
 function drawPlatform() {
+    // Draw the platform that the player controls
+
     ctx.beginPath();
     ctx.rect(platformX, platformY, pWidth, 10);
     ctx.fillStyle = "white";
@@ -73,37 +168,9 @@ function drawPlatform() {
     ctx.closePath();
 }
 
-function keyActionUp(key) {
-    if (key.keyCode == 37) {
-        left = 0;
-    }
-    else if (key.keyCode == 39) {
-        right = 0;
-    }
-}
-
-function keyActionDown(key) {
-    if (key.keyCode == 32) {
-        started = 1;
-        startText.style.display = "none";
-    }
-    if (key.keyCode == 37) {
-        left = 1;
-    }
-    else if (key.keyCode == 39) {
-        right = 1;
-    }
-}
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2*Math.PI);
-    ctx.fillStyle = "blue";
-    ctx.fill();
-    ctx.closePath();
-}
-
 function detectCollision() {
+    // Check if the ball hit a brick and remove the brick if it has
+
     for (let i = 0; i < bricks.length; i++) {
         if (bricks[i][2] == 1) {
             if (x - radius >= bricks[i][0] && x - radius <= bricks[i][0] + bWidth && y - radius >= bricks[i][1] && y - radius <= bricks[i][1] + bHeight) {
@@ -117,7 +184,36 @@ function detectCollision() {
     }
 }
 
+function keyActionUp(key) {
+    // Stop moving the paddle if the left or right key is no longer being pushed
+
+    if (key.keyCode == 37) {
+        left = 0;
+    }
+    else if (key.keyCode == 39) {
+        right = 0;
+    }
+}
+
+function keyActionDown(key) {
+    // Start the game if SPACE pushed
+    // Move the paddle depending on which button was pressed
+
+    if (key.keyCode == 32) {
+        started = 1;
+        startText.style.display = "none";
+    }
+    if (key.keyCode == 37) {
+        left = 1;
+    }
+    else if (key.keyCode == 39) {
+        right = 1;
+    }
+}
+
 function checkWin() {
+    // Return true if all the bricks are destroyed and false otherwise
+
     for (let i = 0; i < bricks.length; i++) {
         if (bricks[i][2] == 1) {
             return false;
@@ -127,6 +223,8 @@ function checkWin() {
 }
 
 function announceWin(win) {
+    // Game over so stop the game, display replay message, and allow player to replay by pressing R
+
     clearInterval(interval);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawPlatform()
@@ -141,62 +239,15 @@ function announceWin(win) {
 
 }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawRectangles();
-    drawPlatform();
-    detectCollision();
-
-    if (started) {
-        if (y + dy + radius == platformY) {
-            if (platformX - 2 * radius < x + dx && x + dx < platformX + pWidth + radius && y + dy + radius == platformY) {
-                dy = -dy;
-                if (dx == 0) {
-                    dx = 2;
-                }
-            }
-        }
-        else {
-            if (x + dx > canvas.width - radius || x + dx < radius) {
-                dx = -dx;
-            }
-            if (y + dy < radius) {
-                dy = -dy;
-            }
-        }
-        if (y >= canvas.height + radius) {
-            announceWin(0)
-        }
-        x += dx;
-        y += dy;
-    }
-    if (left) {
-        platformX = Math.max(platformX - 6, 0);
-    }
-    else if (right) {
-        platformX = Math.min(platformX + 6, canvas.width - pWidth);
-    }
-}
-
 function resetGame(key) {
+    // Reset the game if R key pressed
+
     if (key.keyCode == 82) {
         loseText.style.display = "none";
         winText.style.display = "none";
+        startText.style.display = "block";
+        started = 0
         setGame()
     }
 }
 
-function setGame() {
-    x = canvas.width / 2;
-    y = canvas.height - 200;
-    radius = 10;
-    dx = 0;
-    dy = 4;
-    platformX = canvas.width / 2 - 30;
-    platformY = canvas.height - 18;
-    setBricks()
-    interval = setInterval(draw, 10);
-}
-
-setGame()
